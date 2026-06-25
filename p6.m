52bfe8fff943cf52ff3cdfb45cae2d6d4f28121d
Emanuel <emanuel.trinc@yahoo.com>
2026-06-25 06:58:48 +0300

    Add files via upload

---
+++ p6.m
@@ -0,0 +1,43 @@
+% Variables
+apogee = 45000e3;
+perigee = 7000e3;
+
+% EXERCITIU 1
+% A. Calculați axa semimajoră a orbitei eliptice (folositi formula)
+axaSemimajora = 26000000;   % Folositi formula
+disp("axa semimajora = " + axaSemimajora);
+% B. Calculați excentricitatea orbitei (folositi formula)
+eccentricity = 0.73;    % Folositi formula
+disp("Eccentricity: " + eccentricity);
+
+% Define Satellite Scenario
+startTime = datetime(2020, 5, 1, 11, 36, 0);
+stopTime = startTime + days(1);
+sampleTime = 60;
+scenario = satelliteScenario(startTime, stopTime, sampleTime);
+
+inclination = 0;
+rightAscensionOfAscendingNode = 0;
+argumentOfPeriapsis = 0;
+trueAnomaly = 0;
+satA = satellite(scenario, ...
+    axaSemimajora, ...
+    eccentricity, ...
+    inclination, ...
+    rightAscensionOfAscendingNode, ...
+    argumentOfPeriapsis, ...
+    trueAnomaly, ...
+    "Name", "Satelit A");
+
+% EXERCITIU 2 - Creare Satelit B orbita circulara
+
+% EXERCITIU 3 - Creare Statie Terestra
+gs = groundStation(scenario, ...
+    "Name", "Ground Station", ...
+    "Latitude", 10, ...
+    "Longitude", -30);
+
+access(satA, gs);
+
+viewer = satelliteScenarioViewer(scenario);
+play(scenario);
